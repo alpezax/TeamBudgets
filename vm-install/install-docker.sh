ef508da0d5df239f7d13d6ebda85563fcc4e83e4
alpezax <alpezax@gmail.com>
2025-05-14 00:19:09 +0200

    docker
    docker

---
+++ vm-install/install-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# --------------------------------------
+# Script para instalar Docker y Docker Compose
+# en una VM Ubuntu/Debian (como e2-micro en GCP)
+# --------------------------------------
+set -e
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+docker --version
+
